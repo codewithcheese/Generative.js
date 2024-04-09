4983e47da8148015d0ad4f4313c10b82ffd1c98a
Tom Manning <mr.tmanning@gmail.com>
2024-04-09 22:31:39 +1000

    Add doc site using nextra (#7)

---
+++ sites/generativejs.dev/theme.config.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Generative.js</span>,
+  project: {
+    link: "https://github.com/codewithcheese/Generative.js",
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: "https://github.com/codewithcheese/Generative.js",
+  footer: {
+    text: "",
+  },
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+};
+
+export default config;
