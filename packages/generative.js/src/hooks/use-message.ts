9bb0c24ad44b110956a97180f245c258f1e09c06
codewithcheese <mr.tmanning@gmail.com>
2024-04-12 18:17:34 +1000

    Clarify error

--- packages/generative.js/src/hooks/use-message.ts
+++ packages/generative.js/src/hooks/use-message.ts
@@ -5,7 +5,7 @@ export function useMessage() {
   const context = useContext(MessageContext);
   if (context == null) {
     throw Error(
-      "Cannot access message. User of useMessage() must be wrapped by a message provider. e.g. System, User, Assistant",
+      "Cannot access message. Usage of useMessage() must be wrapped by a message provider. e.g. System, User, Assistant",
     );
   }
   return context.message;
