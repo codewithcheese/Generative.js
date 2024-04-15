47c196c3e5b84f206cc957070f3ab41a972d5b2f
codewithcheese <mr.tmanning@gmail.com>
2024-04-15 23:04:51 +1000

    Fix types for Router

--- packages/generative.js/src/components/Assistant.tsx
+++ packages/generative.js/src/components/Assistant.tsx
@@ -2,6 +2,8 @@ import "../util/readable-stream-polyfill.js";
 import { AnthropicAssistant, AnthropicAssistantProps } from "../index.js";
 import { OpenaiAssistant, OpenaiAssistantProps } from "./OpenaiAssistant.js";
 
+export type AssistantApi = "openai" | "anthropic";
+
 type AssistantProps<T extends "openai" | "anthropic"> = {
   api: T;
 } & (T extends "openai" ? OpenaiAssistantProps : AnthropicAssistantProps);
